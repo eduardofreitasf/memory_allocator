410451de47572f571ff978c539ff834beadb7878
eduardofreitasf <ef05238@gmail.com>
2025-06-14 11:16:08 +0100

    memory allocator API

---
+++ mem_alloc.h
@@ -0,0 +1,13 @@
+#ifndef MEM_ALLOC_H
+#define MEM_ALLOC_H
+
+/* for size_t */
+#include <stddef.h>
+
+void * mem_alloc(size_t size);
+
+void mem_free(void * ptr);
+
+void * mem_resize(void * prt, size_t size);
+
+#endif /* MEM_ALLOC_H */
