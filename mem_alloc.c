22c43d03ce5b378cc8044ec34dbb6c06035d5b18
eduardofreitasf <ef05238@gmail.com>
2025-06-26 16:15:25 +0100

    memory allocator working

--- mem_alloc.c
+++ mem_alloc.c
@@ -1,340 +1,431 @@
 
 #include "mem_alloc.h"
-
 #include <unistd.h>
 #include <string.h>
-
-/**
- * Allocation bit:
- * - 1 == allocated
- * - 0 == free
- */
-
-/**
- * @brief Number of metadata words stored in a memory block
- * 
- */
-#define NR_WORDS 4
-
-/**
- * @brief Minimum size of memory block
- * 
- */
-#define MIN_SIZE (NR_WORDS * sizeof(size_t))
+#include <stdio.h>
 
 
-/**
- * @brief Holds the memory adress of the start of the heap
- * 
- * Constant value at all times
- * 
- */
+#define PTR_SIZE (sizeof(void *))
+#define WORD_SIZE (sizeof(size_t))
+
+#define MIN_SIZE (4 * WORD_SIZE)
+#define MIN_PAYLOAD (2 * WORD_SIZE)
+
+
 static void * heap_start = NULL;
-
-/**
- * @brief Holds the memory adress of the end of the heap
- * 
- * Updated every time sbrk() is used
- * 
- */
 static void * heap_end = NULL;
-
-/**
- * @brief Holds the memory adress of the first free block
- */
 static void * free_list = NULL;
 
 
 /**
- * @brief Initializes the heap start
- * 
+ * ===========================================
+ *                  UTILS
+ * ===========================================
  */
-static void init_heap_start(void) {
+
+static void init_heap(void) {
     heap_start = sbrk(0);
     heap_end = heap_start;
 }
 
-
-/**
- * @brief Removes the block pointed by ptr from the free list
- * 
- * @param ptr pointer to a block of memory
- */
-static void fl_remove(void * ptr) {
-    void * temp = ptr + sizeof(size_t);
-    // update the previous pointer in the next block
-    memcpy(&temp + 2 * sizeof(size_t), ptr + 2 * sizeof(size_t), sizeof(void *));
-
-    temp = ptr + 2 * sizeof(size_t);
-    // update the next pointer in the previous block
-    memcpy(&temp + sizeof(size_t), ptr + sizeof(size_t), sizeof(void *));
+static size_t adjust_size(size_t size) {
+    if (size < MIN_PAYLOAD) {
+        size = MIN_PAYLOAD;
+    }
+    return 2 * WORD_SIZE + ((size + 7) & ~7);
 }
 
 
-/**
- * @brief Adds the block pointed by ptr to the free list
- * 
- * @param ptr pointer to a block of memory
+
+/*
+ * ===========================================
+ *             EXPLICIT FREE LIST
+ * ===========================================
  */
-static void fl_add(void * ptr);
 
+static void free_list_add(void * ptr) {
 
-/**
- * @brief Joins the memory block pointed by ptr to it's neighbours
- * 
- * @param ptr pointer to a block of memory
- */
-static void coalesce(void * ptr);
+    // empty free list
+    if (free_list == NULL) {
+        free_list = ptr;
+        // place the next pointer
+        memcpy(ptr + WORD_SIZE, &ptr, PTR_SIZE);
+        // place the previous pointer
+        memcpy(ptr + 2 * WORD_SIZE, &ptr, PTR_SIZE);
+    } else {
+        // update the next pointer on the new block
+        memcpy(ptr + WORD_SIZE, &free_list, PTR_SIZE);
+        // update the previous pointer on the new block
+        memcpy(ptr + 2 * WORD_SIZE, free_list + 2 * WORD_SIZE, PTR_SIZE);
 
+        // get the last element on the list
+        void * last = NULL;
+        memcpy(&last, free_list + 2 * WORD_SIZE, PTR_SIZE);
 
-/**
- * @brief Iterates over the free list to find a free block with size bytes
- * 
- * This search uses a Best Fit algorithm to find a free block,
- * so the whole list is searched.
- * 
- * @param size number of bytes for the block
- * @return memory adress of a block with at least size byes
- * @retval NULL if there isn't a block with size bytes
- */
+        // update the next pointer on the last element
+        memcpy(last + WORD_SIZE, &ptr, PTR_SIZE);
+
+        // update the previous pointer on the list head
+        memcpy(free_list + 2 * WORD_SIZE, &ptr, PTR_SIZE);
+
+        // update the head of the list
+        free_list = ptr;
+    }
+
+}
+
+static void free_list_remove(void * ptr) {
+
+    void * next = NULL, * previous = NULL;
+    // get the next block
+    memcpy(&next, ptr + WORD_SIZE, PTR_SIZE);
+    // get the previous block
+    memcpy(&previous, ptr + 2 * WORD_SIZE, PTR_SIZE);
+
+    // list has only one element
+    if (next == ptr) {
+        free_list = NULL;
+    } else {
+        // ptr is the head of the list
+        if (free_list == ptr) {
+            free_list = next;
+        }
+
+        // update the next pointer on the previous block
+        memcpy(previous + WORD_SIZE, &next, PTR_SIZE);
+        // update the previous pointer on the next block
+        memcpy(next + 2 * WORD_SIZE, &previous, PTR_SIZE);
+    }
+
+}
+
 static void * find_free_block(size_t size) {
-    // free list is empty
     if (free_list == NULL) {
         return NULL;
     }
 
+    void * iterator = free_list;
     void * start = free_list;
     void * best_fit = NULL;
-    size_t block_size = 0, best_size = 0;
 
-    /*
-      the block_size and best_size variables are not necessary,
-      but I use them for better readability. 
-      I could just do: *(size_t *)start
-     */
-
-    // iterate over the free list
     do {
-        // read the block size
-        memcpy(&block_size, start, sizeof(block_size));
-
-        // check if the block size is enough
-        if (block_size >= size) {
-            // best_fit == NULL -> first time finding a block with valid size
-            // block_size < best_size -> found a block with a better match then the previous best fit
-            if (best_fit == NULL || block_size < best_size) {
-                best_fit = start;
-                best_size = block_size;
+        // block has a valid size
+        if (*(size_t *)iterator >= size) {
+            // found a better block
+            if (best_fit == NULL || *(size_t *)iterator < *(size_t *)best_fit) {
+                best_fit = iterator;
             }
         }
-
-        // place the next pointer in start
-        memcpy(start, start + sizeof(size_t), sizeof(void *));
-
-    } while (start != free_list);
-
-    // found a valid memory block
-    if (best_fit != NULL) {
-        // a smaller block will be formed
-        if (best_size > size && (best_size - size) >= MIN_SIZE) {
-            // remaining piece of the block
-            size_t remaining = best_size - size;
-
-            // change the value of the block size in the footer
-            memcpy(best_fit + best_size - sizeof(best_size), &remaining, sizeof(remaining));
-        
-            // change the value of the block size in the header
-            memcpy(best_fit + size, &remaining, sizeof(remaining));
-
-
-            /* update the next and previous pointers in all blocks */
-
-            // new block address
-            void * new_block = best_fit + best_size;
-
-            // place the next pointer in the new block
-            memcpy(new_block + sizeof(size_t), best_fit + sizeof(size_t), sizeof(void *));
-
-            // update the previous pointer in the next free list element
-            void * next = best_fit + sizeof(size_t);
-            memcpy(&next + 2 * sizeof(size_t), &new_block, sizeof(void *));
-
-            // update the next pointer in the best fit
-            memcpy(best_fit + sizeof(size_t), &new_block, sizeof(void *));
-        }
-
-
-        /* remove the block from the free list */
-        fl_remove(best_fit);
-    }
+    
+        // go to the next free block
+        memcpy(&iterator, iterator + WORD_SIZE, sizeof(void *));
+    } while (iterator != start);
 
     return best_fit;
 }
 
 
-/**
- * @brief Determines the size for a valid block, with atleast size bytes
- * 
- * Minimum size for a block is MIN_SIZE
- * 
- * @param size number of bytes requested by the user
- * @return valid block size in bytes
+
+/*
+ * ===========================================
+ *                COALESCING
+ * ===========================================
  */
-static size_t adjust_size(size_t size) {
-    if (size < MIN_SIZE) {
-        return MIN_SIZE;
+
+/**
+ * @brief 
+ * 
+ * Assumes it is not the end of the heap
+ * 
+ * @param ptr 
+ * @return void* 
+ */
+static void * coalesce_right(void * ptr, char * coalesce) {
+
+    // get the block on the right
+    void * next = ptr + *(size_t *) ptr;
+    size_t next_size = *(size_t *) next;
+
+    // check if the right is free
+    if ((next_size & 1) == 0) {
+
+        *coalesce = 1;
+
+        // update the size of the block
+        next_size += *(size_t *)ptr;
+        // update the header
+        memcpy(ptr, &next_size, WORD_SIZE);
+        // update the footer
+        memcpy(ptr + (next_size & ~1) - WORD_SIZE, &next_size, WORD_SIZE);
+
+        // remove the next block from the free list
+        free_list_remove(next);
+
+        // add the block to the free list
+        free_list_add(ptr);
     }
 
-    return (size + (2 * sizeof(size_t)) + 7) & ~7;
+    return ptr;
+}
+
+/**
+ * @brief 
+ * 
+ * Assumes it is not the heap start
+ * 
+ * @param ptr 
+ * @return void* 
+ */
+static void * coalesce_left(void * ptr, char * coalesce) {
+
+    // get the block on the left
+    size_t prev_size = *(size_t *)(ptr - WORD_SIZE);
+    void * previous = ptr - prev_size;
+
+    // check if the left is free
+    if ((prev_size & 1) == 0) {
+
+        *coalesce = 1;
+
+        // update the size of the block
+        prev_size += *(size_t *) ptr;
+        // update the header
+        memcpy(previous, &prev_size, WORD_SIZE);
+        // update the footer
+        memcpy(previous + (prev_size & ~1) - WORD_SIZE, &prev_size, WORD_SIZE);
+
+        ptr = previous;
+    }
+
+    return ptr;
 }
 
 
+
+/**
+ * ===========================================
+ *               API FUNCTIONS
+ * ===========================================
+ */
+
+
 void * mem_alloc(size_t size) {
-    static char first = 0;
-    // start the heap, on the first call to mem_alloc()
-    if (first == 0) {
-        init_heap_start();
-        first = 1;
+
+    static char init = 0;
+    // first call to mem_alloc()
+    if (init == 0) {
+        // start the heap
+        init_heap();
+        init = 1;
     }
-    
+
     if (size == 0) {
         return NULL;
     }
 
-    // get a valid block size
+
+    // size cannot be higher than PTRDIFF_MAX
+    // consider RLIMIT_DATA, but first check if it's equal to RLIMIT_INFINITY
+    
+
+    // adjust the size of the block
     size = adjust_size(size);
 
-    // find a free block
+    size_t remain_size = 0;
     void * free_block = find_free_block(size);
-
-    // use sbrk() to increment the program break
+    // there are no free blocks with size bytes
     if (free_block == NULL) {
+
+        // increment the program break
         free_block = sbrk(size);
 
-        // update the heap end
+        if (free_block == (void *) -1) {
+
+            // sbrk() failed
+
+            return NULL;
+        }
+        
+        // update the end of the heap
         heap_end = sbrk(0);
+
+    } else {
+        // remove the free block from the free list
+        free_list_remove(free_block);
+        // get the remaining size of the block
+        remain_size = *(size_t *)free_block - size;
+
+        // create a block with the remaining space
+        if (remain_size >= MIN_SIZE) {
+            void * remain = free_block + size;
+            // place the header on the remaining block
+            memcpy(remain, &remain_size, WORD_SIZE);
+            // update the footer on the remaining block
+            memcpy(remain + remain_size - WORD_SIZE, &remain_size, WORD_SIZE);
+
+            // add the remaining block to the free list
+            free_list_add(remain);
+        } else {
+            // update the size
+            size = *(size_t *)free_block;
+        }
     }
 
-    // set the allocation bit to 1
-    size += 1;
+    // set the block as allocated
+    size |= 1;
+    // update the header of the block
+    memcpy(free_block, &size, WORD_SIZE);
+    // update the footer on the free block
+    memcpy(free_block + (size & ~1) - WORD_SIZE, &size, WORD_SIZE);
 
-    // place the block size in the header
-    memcpy(free_block, &size, sizeof(size));
-    
-    // place the block size and allocation bit in the footer
-    // copy from the header
-    memcpy(free_block + size - 1 - sizeof(size), free_block, sizeof(size));
-
-    // return the address of the payload
-    return free_block + sizeof(size);
+    return free_block + WORD_SIZE;
 }
 
 
 void mem_free(void * ptr) {
-    // check if the heap is initiated
-    if (heap_start == NULL) {
-        
-        /* SEND SOME SIGNAL (HEAP IS OFF) */
 
-        return;
-    }
-
-    // ptr is not a heap memory adress
     if (ptr <= heap_start || ptr >= heap_end) {
 
-        /* SEND SOME SIGNAL (INVALID POINTER) */
+        // signal INVALID POINTER
 
         return;
     }
 
-    // pointer to the payload now points to the header
-    ptr = ptr - sizeof(size_t);
-    // get the header
-    size_t * header = (size_t *) ptr;
-    
-    /* check if the memory block is allocated */
-    if ((*header & 1) == 0) {
+    // get the header of the block
+    ptr = ptr - WORD_SIZE;
+    size_t size = *(size_t *)ptr;
 
-        /* SEND SOME SIGNAL (DOUBLE FREE) */
+    // block is free
+    if ((size & 1) == 0) {
+
+        // signal DOUBLE FREE
 
         return;
     }
 
-    // set the allocation flag to 0 in the header
-    *header = *header & ~1;
+    // set the allocation bit to zero
+    size &= ~1;
+    // change the header
+    memcpy(ptr, &size, WORD_SIZE);
+    // change the footer
+    memcpy(ptr + size - WORD_SIZE, &size, WORD_SIZE);
 
-    size_t block_size = *header;
-    size_t * footer = (size_t *) (ptr + block_size - sizeof(block_size));
-
-    // set the allocation flag to 0 in the footer
-    *footer = *footer & ~1;
-
-    
-    /* coalescing mechanism */
-
-    size_t * neighbour = NULL;
-    // checks if coalescing was necessary
     char coalesce = 0;
 
-    /* check the next block to coalesce */
-    if (ptr + block_size < heap_end) {
-        neighbour = (size_t *) (ptr + block_size);
-        // next block is free
-        if ((*neighbour & 1) == 0) {
-            coalesce = 1;
-
-            block_size += *neighbour;
-            // change the block size in the header
-            *header = block_size;
-            
-            // change the footer
-            footer = ((void *) footer) + *neighbour;
-            // change the block size in the footer
-            *footer = block_size;
-
-
-            /* FIX THE NEXT AND PREVIOUS POINTERS */
-
-
-        }
-    }
-    
-    /* check the previous block to coalesce */
-    if (ptr - sizeof(block_size) >= heap_start) {
-        neighbour = (size_t *) ptr - sizeof(block_size);
-        // previous block is free
-        if ((*neighbour & 1) == 0) {
-            coalesce = 1;
-
-            block_size += *neighbour;
-
-            // change the block size in the footer
-            *footer = block_size;
-
-            // change the header
-            header = ((void *) header) - *neighbour;
-            // change the block size in the header
-            *header = block_size;
-        }
+    if (ptr + size < heap_end) {
+        ptr = coalesce_right(ptr, &coalesce);
+        size = *(size_t *) ptr;
     }
 
+    if (ptr > heap_start) {
+        if (coalesce == 1) {
+            free_list_remove(ptr);
+        }
 
-    /* add the block to the free list, only if it did NOT coalesce */
+        ptr = coalesce_left(ptr, &coalesce);
+        size = *(size_t *) ptr;
+    }
 
-    // free list empty, create circular list
-    if (free_list == NULL) {
-        free_list = (void *) header;
+    // did not coalesce
+    if (coalesce == 0) {
+        // add the block to the free list
+        free_list_add(ptr);
+    }
 
-        /* SET THE NEXT AND PREVIOUS POINTERS */
+    // last block on the heap
+    if (ptr + size == heap_end) {
+        // remove block from free list
+        free_list_remove(ptr);
+        // decrement the program break
+        sbrk(-size);
 
-    } else if (coalesce != 0) {
-        // add new memory block to the free list
-
-
+        heap_end = sbrk(0);
     }
 
 }
 
 
-void * mem_resize(void * prt, size_t size);
+void * mem_resize(void * ptr, size_t size) {
+
+    void * block = mem_alloc(size);
+    if (block != NULL) {
+        memcpy(block, ptr, size);
+        mem_free(ptr);
+    }
+
+    return block;
+}
+
+
+void * mem_alloc_clear(size_t n, size_t size) {
+
+    void * block = mem_alloc(n * size);
+    if (block != NULL) {
+        memset(block, 0, n * size);
+    }
+
+    return block;
+}
+
+
+static void show_block(void * ptr) {
+
+    size_t size = *(size_t *) ptr;
+
+    printf("------------------------------\n");
+    printf("Address: %p\n", ptr);
+    printf("Status: %s\n", (size & 1) ? "Allocated" : "Free");
+    printf("Block size: %ld\n", size & ~1);
+    printf("Header: %ld\n", *(size_t *)ptr);
+    if ((size & 1) == 0) {
+        void * temp = NULL;
+        memcpy(&temp, ptr + WORD_SIZE, PTR_SIZE);
+        printf("Next: %p\n", temp);
+        memcpy(&temp, ptr + 2 * WORD_SIZE, PTR_SIZE);
+        printf("Previous: %p\n", temp);
+    } else {
+        printf("Payload: ...\n");
+    }
+    printf("Footer: %ld\n", *(size_t *)(ptr + (size & ~1) - WORD_SIZE));
+}
+
+static void show_free_list(void) {
+    if (free_list == NULL) {
+        return;
+    }
+
+    printf("\n============ FREE LIST ============\n");
+
+    void * iterator = free_list;
+    do {
+        show_block(iterator);
+        memcpy(&iterator, iterator + WORD_SIZE, PTR_SIZE);
+    } while (iterator != free_list);    
+}
+
+
+void show_heap(void) {
+    if (heap_start == NULL) {
+        printf("HEAP is NULL\n");
+        return;
+    }
+
+    printf("\n============ HEAP ============\n");
+    
+    printf("START: %p\n", heap_start);
+    printf("END: %p\n", heap_end);
+    printf("HEAP SIZE: %ld\n", (size_t) (heap_end - heap_start));
+    
+    void * temp = heap_start;
+    size_t offset = 0;
+    
+    while (temp != heap_end) {
+        offset = *(size_t *)temp;
+        show_block(temp);
+        temp += (offset & ~1);    
+    }
+    
+    printf("==============================\n");
+
+}
 
--- mem_alloc.h
+++ mem_alloc.h
@@ -10,6 +10,20 @@ void * mem_alloc(size_t size);
 
 void mem_free(void * ptr);
 
-void * mem_resize(void * prt, size_t size);
+void * mem_resize(void * ptr, size_t size);
+
+void * mem_alloc_clear(size_t n, size_t size);
+
+/* shows the allocated and free blocks */
+/* used only for debugging purposes */
+void show_heap(void);
+
+
+/*
+    WARNING:
+    FIND A WAY OF AVOIDING THIS:
+        mem_free(ptr + 1);
+*/
+
 
 #endif /* MEM_ALLOC_H */
